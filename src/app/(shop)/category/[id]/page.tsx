2b3034bed30804093d841f542838552cc3f7258c
John A. Quintero <quinteroberrio@gmail.com>
2024-02-23 23:59:08 -0500

    categories was completed

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,23 +1,35 @@
+import ProductGrid from "@/components/products/product-grid/ProductGrid";
+import Title from "@/components/ui/title/Title";
+import { Product } from "@/interfaces";
+import { Category, initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
 export default function ({ params }: Props) {
   const { id } = params;
-  let render!: JSX.Element;
-  switch (id) {
-    case "men":
-      return <div>Category {id}</div>;
-    case "women":
-      return <div>Category {id}</div>;
-    case "kids":
-      return <div>Category {id}</div>;
-
-    default:
-      notFound();
-  }
+  const products = initialData.products;
+  const translate: Record<Category, string> = {
+    men: "para Hombres",
+    women: "para Mujeres",
+    kid: "para Niños",
+    unisex: "para Todos",
+  };
+  const filteredProducs = products.filter((product) => product.gender === id);
+  return (
+    <>
+      <div>
+        <Title
+          title={`Artículos ${translate[id]}`}
+          subtitle="Todos los productos"
+          className="mb-2"
+        />
+      </div>
+      <ProductGrid products={filteredProducs} />
+    </>
+  );
 }
