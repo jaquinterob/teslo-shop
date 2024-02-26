48b20aabc40faeba66c5fe560c03cba2910222ad
John A. Quintero <quinteroberrio@gmail.com>
2024-02-25 23:18:29 -0500

    padding was added to mobile scrren

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -7,7 +7,7 @@ export default function ShopLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen p-2 md:p-0">
       <TopMenu />
       <Sidebar />
       <div className="px-o sm:px-10">{children}</div>
